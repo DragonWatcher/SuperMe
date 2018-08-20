8602f27ca92f7c084fb2339b6b8d71991e464ffb
mht <952326644@qq.com>
2018-08-20 11:12:22 +0800

    权限模块与预定模块整合调整

--- src/main/java/com/ahav/ReserveApplication.java
+++ src/main/java/com/ahav/ReserveApplication.java
@@ -1,5 +1,8 @@
 package com.ahav;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +10,16 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.ahav.system.PermissionApplication;
+
 @SpringBootApplication
+@MapperScan("com.ahav.system.dao")//扫描 mybatis 包
 public class ReserveApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ReserveApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReserveApplication.class, args);
+		logger.info("======================启动完成=====================");
 	}
 	@Autowired
 	private RestTemplateBuilder builder;
