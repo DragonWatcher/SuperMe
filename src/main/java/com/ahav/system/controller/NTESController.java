a5987aff6d5d53e74fea54a4bd258ae51e64b2f0
mht <952326644@qq.com>
2018-08-21 10:12:02 +0800

    controller接口文档说明

--- src/main/java/com/ahav/system/controller/NTESController.java
+++ src/main/java/com/ahav/system/controller/NTESController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ahav.system.service.LoginService;
 import com.ahav.system.util.SystemConstant;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("网易接口：牟昊天")
 @RestController
 @RequestMapping("/ntes")
 public class NTESController {
@@ -22,6 +26,7 @@ public class NTESController {
      * <br>作者： mht<br> 
      * 时间：2018年8月21日-上午10:04:51<br>
      */
+    @ApiOperation(value = "单点登录接口", notes = "单点登录跳转，默认登录账号：haotian.mou@ahav.com.cn")
     @GetMapping("/login")
     public void ntesLogin(HttpServletResponse response) {
         loginService.ntesLogin(null, response);
