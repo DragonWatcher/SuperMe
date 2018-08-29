f9010dfc259ebe91f0e02e1de12c51fc443c0222
mht <952326644@qq.com>
2018-08-29 17:01:36 +0800

    service注解追加

--- src/main/java/com/ahav/system/service/impl/NtesServiceImpl.java
+++ src/main/java/com/ahav/system/service/impl/NtesServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import com.ahav.system.dao.DeptDao;
 import com.ahav.system.entity.SystemResult;
@@ -14,6 +15,7 @@ import com.ahav.system.service.NtesService;
 import com.ahav.system.util.SystemConstant;
 import com.alibaba.fastjson.JSONObject;
 
+@Service
 public class NtesServiceImpl implements NtesService {
     
     private static final Logger logger = LoggerFactory.getLogger(NtesServiceImpl.class);
