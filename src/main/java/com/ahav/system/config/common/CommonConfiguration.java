b36b145c334f534a3ade9ad3447074529a285c5e
mht <952326644@qq.com>
2018-09-06 15:01:26 +0800

    filter实现跨域解决

--- src/main/java/com/ahav/system/config/common/CommonConfiguration.java
+++ src/main/java/com/ahav/system/config/common/CommonConfiguration.java
@@ -1,15 +1,22 @@
 package com.ahav.system.config.common;
 
-import javax.servlet.MultipartConfigElement;
+import java.io.IOException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.Filter;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CommonConfiguration extends WebMvcConfigurerAdapter {
+public class CommonConfiguration extends Filter {
 
     /**
      * 文件上传配置，在application配置文件中设置不起作用！
@@ -23,13 +30,26 @@ public class CommonConfiguration extends WebMvcConfigurerAdapter {
         factory.setMaxRequestSize("102400KB");
         return factory.createMultipartConfig();
     }
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("header1", "header2", "header3")
-                .allowCredentials(true);
+
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    // registry.addMapping("/**")
+    // .allowedOrigins("*")
+    // .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+    // .allowedHeaders("header1", "header2", "header3")
+    // .allowCredentials(true);
+    // }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest reqs = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
     }
 }
