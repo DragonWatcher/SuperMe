ece1c07b53a888f464a4b414e437dad809831e32
mht <952326644@qq.com>
2018-09-06 11:51:47 +0800

    跨域的全局配置

--- src/main/java/com/ahav/system/config/common/CommonConfiguration.java
+++ src/main/java/com/ahav/system/config/common/CommonConfiguration.java
@@ -5,9 +5,12 @@ import javax.servlet.MultipartConfigElement;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CommonConfiguration {
+public class CommonConfiguration extends WebMvcConfigurationSupport {
 
     /**
      * 文件上传配置，在application配置文件中设置不起作用！
@@ -20,5 +23,13 @@ public class CommonConfiguration {
         // 设置总上传数据总大小
         factory.setMaxRequestSize("102400KB");
         return factory.createMultipartConfig();
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowCredentials(true);
     }
 }
