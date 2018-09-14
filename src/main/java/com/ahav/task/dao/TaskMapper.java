0ee863c98620fdc5f5fabe03ea2e5589eb198a0b
wxh <xiaohui.wang@ahav.com.cn>
2018-09-14 16:42:22 +0800

    阶段性备份

--- src/main/java/com/ahav/task/dao/TaskMapper.java
+++ src/main/java/com/ahav/task/dao/TaskMapper.java
@@ -1,6 +1,12 @@
 package com.ahav.task.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.ahav.task.entity.Query;
 import com.ahav.task.entity.Task;
 
 @Mapper
@@ -16,4 +22,12 @@ public interface TaskMapper {
     int updateByPrimaryKeySelective(Task record);
 
     int updateByPrimaryKey(Task record);
+
+	List<Task> findTasks(@Param("params") Map query);
+
+	Integer findCounts(@Param("params") Map query);
+
+//	List<Task> findTasks(Integer pageSize, Integer currentPage, String orderBy, String publisher, String surveyor,
+//			String executor, String taskStatus, String acceptanceResults, String relatedMeetings, String endTime1,
+//			String endTime2, String taskName, String publishTime1, String publishTime2, String keyWord);
 }
